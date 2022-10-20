bff74d10c6b54bc2a9b919f143ee86e6a6ac2a96
Altti Mäkelä <altti.makela@tuni.fi>
2022-10-20 14:33:52 +0300

    Bargraph added

--- rojekti/creategraph.cpp
+++ rojekti/creategraph.cpp
@@ -7,7 +7,8 @@
 
 // Widget used to display charts
 #include <QtCharts/QChartView>
-
+#include <QBarSeries>
+#include <QBarSet>
 using namespace std;
 
 class CreateGraph {
@@ -23,14 +24,35 @@ void drawGraph(vector<int> xaxis,vector<int> yaxis, QFrame* object)
         for (int i = 0; i< yaxis.size() ; i++){
             axisy->append(xaxis[i], yaxis[i]);
         }
-        // 3. Line chart example
-        // Other options here https://doc.qt.io/qt-5.11/qtcharts-customchart-example.html
-
 
         // Create chart, add data, hide legend, and add axis
         QChart *chart = new QChart();
         chart->legend()->hide();
         chart->addSeries(axisy);
+        chart->createDefaultAxes();
+
+
+        QChartView *graph = new QChartView(chart);
+        graph->resize(object->width(),object->height());
+        graph->setParent(object);
+
+};
+void drawBarGraph(map<int, int> yaxis, QFrame* object)
+{
+
+    //QValueAxis *axisX = new QValueAxis();
+    //axisX->setRange(xaxis[0], xaxis[xaxis.size()-1]+1);
+    QBarSet *axisy = new QBarSet("messages");
+
+        for (int i = 0; i< yaxis.size() ; i++){
+            axisy->append(yaxis[i]);
+        }
+    QBarSeries *axisy2 = new QBarSeries();
+    axisy2->append(axisy);
+        // Create chart, add data, hide legend, and add axis
+        QChart *chart = new QChart();
+        chart->legend()->hide();
+        chart->addSeries(axisy2);
         chart->createDefaultAxes();
 
 
