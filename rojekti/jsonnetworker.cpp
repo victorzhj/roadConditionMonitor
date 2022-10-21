e97ab52088e71bbb0fed4f42df1a849caeecee6b
Joni Niemelä <joni.niemela@tuni.fi>
2022-10-21 21:26:28 +0300

    Networkeria aloitettu

--- rojekti/jsonnetworker.cpp
+++ rojekti/jsonnetworker.cpp
@@ -1,7 +1,27 @@
 #include "jsonnetworker.h"
 #include <QNetworkAccessManager>
 
-jsonNetworker::jsonNetworker()
+
+
+jsonNetworker::jsonNetworker(QObject *parent = nullptr) : QObject(parent)
+{
+    connect(&manager, &QNetworkAccessManager::sslErrors, this, &jsonNetworker::sslErrors);
+}
+
+void jsonNetworker::get(QString location)
+{
+    //QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    //connect(manager, &QNetworkAccessManager::finished, this, &jsonNetworker::downloadFinished);
+}
+
+void jsonNetworker::readyRead()
 {
 
 }
+
+void jsonNetworker::finished(QNetworkReply *reply)
+{
+
+}
+
+
