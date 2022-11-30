0c0751886738455facf561988dc0d42f804491b6
Joni Niemelä <joni.niemela@tuni.fi>
2022-11-30 21:00:58 +0200

    Comments added and getWeatherForecast modified to use all 4 coordinates.

--- rojekti/urlbuilder.cpp
+++ rojekti/urlbuilder.cpp
@@ -4,7 +4,13 @@ urlBuilder::urlBuilder()
 {
 
 }
-
+/**
+ * @brief urlBuilder::getRoadMaintenanceUrl builds URL to get road maintenance from digitraffic API
+ * @param taskName what road maintenance task is wanted. If empty, get all
+ * @param coords contains min and max coordinates from within the data is being collected
+ * @param date from what day the data is wanted
+ * @return finished URL
+ */
 QUrl urlBuilder::getRoadMaintenanceUrl(QString taskName, QVector<QString> coords, QString date)
 {
       QString url = digitrafficBase + "maintenance/v1/tracking/routes?endFrom="
@@ -17,7 +23,11 @@ QUrl urlBuilder::getRoadMaintenanceUrl(QString taskName, QVector<QString> coords
       if (taskName != "") url = url + "&taskId=" + taskName;
       return QUrl(url);
 }
-
+/**
+ * @brief urlBuilder::getRoadConditionUrl build URL to get road conditions from digitraffic API
+ * @param coords contains min and max coordinates from within the data is being collected
+ * @return finished URL
+ */
 QUrl urlBuilder::getRoadConditionUrl(QVector<QString> coords)
 {
     QString url = digitrafficBase + "v3/data/road-conditions/"
@@ -25,13 +35,21 @@ QUrl urlBuilder::getRoadConditionUrl(QVector<QString> coords)
                 + "/" + coords[2] + "/" + coords[3];
     return QUrl(url);
 }
-
+/**
+ * @brief urlBuilder::getTrafficMsgUrl builds URL to get traffic messages from digitraffic API
+ * @return finished URL
+ */
 QUrl urlBuilder::getTrafficMsgUrl()
 {
     QString url = digitrafficBase + "traffic-message/v1/messages?inactiveHours=0&includeAreaGeometry=false&situationType=TRAFFIC_ANNOUNCEMENT";
     return QUrl(url);
 }
-
+/**
+ * @brief urlBuilder::getWeatherObservations builds URL to get road maintenance from digitraffic API
+ * @param coords contains min and max coordinates from within the data is being collected
+ * @param param choose what information is wanted
+ * @return finished URL
+ */
 QUrl urlBuilder::getWeatherObservations(QVector<QString> coords, QString param)
 {
     QString url = fmiBase + "observations::weather::simple&bbox="
@@ -40,7 +58,14 @@ QUrl urlBuilder::getWeatherObservations(QVector<QString> coords, QString param)
             +"&timestep=60&parameters=" + param;
     return QUrl(url);
 }
-
+/**
+ * @brief urlBuilder::getAveragegObservations builds URL to get hourly road maintenance from FMI API
+ * @param startDate from what day the query starts
+ * @param endDate to what day the query ends
+ * @param coords contains min and max coordinates from within the data is being collected
+ * @param param choose what information is wanted, for example average air temp is "t2m"
+ * @return finished URL
+ */
 QUrl urlBuilder::getAveragegObservations(QString startDate, QString endDate, QVector<QString> coords, QString param)
 {
     QString url = fmiBase + "observations::weather::hourly::simple&bbox="
@@ -52,15 +77,22 @@ QUrl urlBuilder::getAveragegObservations(QString startDate, QString endDate, QVe
             + "&parameters=" + param;
     return QUrl(url);
 }
-
+/**
+ * @brief urlBuilder::getWeatherForecast builds URL to get hourly weather forecast from FMI API
+ * @param date from what day the data is wanted
+ * @param coords contains min and max coordinates from within the data is being collected
+ * @param param choose what information is wanted
+ * @return finished URL
+ */
 QUrl urlBuilder::getWeatherForecast(QString date, QVector<QString> coords, QString param)
 {
     QString url = fmiBase
-            + "forecast::harmonie::surface::point::simple&latlon="
+            + "forecast::harmonie::surface::point::simple&bbox="
             + coords[0] + ","  + coords[1]
+            + "," + coords[2] + "," + coords[3]
             + "&timestep=60&starttime="
             + date + "T00:00:00Z"
-            + "&endtime=" + date + "T" // päivään pitää lisätä yksi, miten?
+            + "&endtime=" + date + "T"
             + "23:59:59Z"
             + "&parameters=" + param;
     return QUrl(url);
